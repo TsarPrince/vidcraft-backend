99819f3aa590d47c6a9d62e6d7e8e9d7519bffab
Prince Singh <marvelousprince012233@gmail.com>
2023-06-07 16:54:26 +0530

    gudoldcors

--- index.ts
+++ index.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import dotenv from 'dotenv'
 
 const app = express()
-dotenv.config()
-
+const cors = require('cors')
 const PORT = process.env.PORT || 5000
+
+dotenv.config()
+app.use(cors())
 app.use(express.json())
 
 require("./routes/hello.routes")(app)
