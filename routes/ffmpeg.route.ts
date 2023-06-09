75413244c59c4f2e83d10bcbdc7e5b5e46e78243
Prince Singh <marvelousprince012233@gmail.com>
2023-06-09 13:56:44 +0530

    removed unnecessay multer code

--- routes/ffmpeg.route.ts
+++ routes/ffmpeg.route.ts
@@ -1,29 +1,7 @@
 import { Application } from 'express'
 import ffmpegController from '../controllers/ffmpeg.controller'
-import multer from 'multer'
-
-import { MAX_FILE_SIZE } from '../constants/multer.constant'
-
-const upload = multer({
-  limits: { fileSize: MAX_FILE_SIZE * 1024 * 1024 }, // Individual fileSize should be <= 25MB
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'uploads/')
-    },
-    filename: (req, file, cb) => {
-      const fileName = file.originalname.slice(0, -4) + '-' + Date.now() + '.mp4'
-      cb(null, fileName)
-    },
-  }),
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype !== 'video/mp4') {
-      return cb(new Error('Only video files are allowed!'))
-    }
-    cb(null, true)
-  }
-})
 
 module.exports = (app: Application) => {
-  app.post('/api/merge', upload.fields([{ name: 'preFile', maxCount: 1 }, { name: 'inputFile', maxCount: 1 }]), ffmpegController.mergeVideo)
-  app.post('/api/getMetadata', upload.single('video'), ffmpegController.getMetadata)
+  app.post('/api/merge', ffmpegController.mergeVideo)
+  app.post('/api/getMetadata', ffmpegController.getMetadata)
 }
--- routes/supabase.route.ts
+++ routes/supabase.route.ts
@@ -1,8 +1,23 @@
 import { Application } from 'express'
 import multer from 'multer'
-const upload = multer({ dest: 'uploads/' })
 import supabaseController from '../controllers/supabase.controller'
 
+import { MAX_FILE_SIZE } from '../constants/multer.constant'
+
+const upload = multer({
+  limits: { fileSize: MAX_FILE_SIZE * 1024 * 1024 }, // Individual fileSize should be <= 25MB
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/')
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'video/mp4') {
+      return cb(new Error('Only video files are allowed!'))
+    }
+    cb(null, true)
+  }
+})
 
 module.exports = (app: Application) => {
   app.post('/api/upload', upload.single('video'), supabaseController.uploadVideo)
